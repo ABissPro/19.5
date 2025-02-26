d71367becf91fb3de2e4f5737dd65796ac36f7c3
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-26 21:04:28 +0300

    Add files via upload

---
+++ 2. Просмотрщик текстовых файлов.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::string filePath;
+
+    std::cout << "Enter the path to the text file: ";
+    std::getline(std::cin, filePath);
+
+    std::ifstream file(filePath);
+
+    if (!file.is_open()) {
+        std::cerr << "Error opening file." << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+
+    file.close();
+}
