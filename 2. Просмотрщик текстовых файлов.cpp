518e619845a864f51bfc2cbc34a477c40346d90e
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-08-24 17:55:56 +0300

    Update 2. Просмотрщик текстовых файлов.cpp

--- 2. Просмотрщик текстовых файлов.cpp
+++ 2. Просмотрщик текстовых файлов.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <string>
 
@@ -8,17 +8,28 @@ int main() {
     std::cout << "Enter the path to the text file: ";
     std::getline(std::cin, filePath);
 
-    std::ifstream file(filePath);
+    std::ifstream file(filePath, std::ios::binary);
 
     if (!file.is_open()) {
         std::cerr << "Error opening file." << std::endl;
         return 1;
     }
 
-    std::string line;
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl;
+    constexpr size_t BUFFER_SIZE = 1024; //Размер буфера для временного хранения
+    char buffer[BUFFER_SIZE];
+
+    while (file.read(buffer, BUFFER_SIZE)) {
+        std::cout.write(buffer, file.gcount());
+    }
+
+    //Обработка оставшейся части файла, если её размер менее BUFFER_SIZE
+    if (!file.eof() && !file.fail()) {
+        file.read(buffer, BUFFER_SIZE);
+        std::cout.write(buffer, file.gcount());
     }
 
     file.close();
+
+    return 0;
 }
+
